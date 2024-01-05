2d98e52b22545afe145d2999234dd289fa847a66
Timothee Kabwe <timothekabwe@gmail.com>
2024-01-05 21:27:02 +0200

    Add description on the new columns

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Timothée Kabwe")
+    content = """
+    Hi, I am Timothy Kabwe, a systems and Network Administration Engineer from Salama
+    Higher Institute of Computer Science(ESISALAMA - DON BOSCO University, Democratic Republic of Congo).
+    Python is my programming language of choice, and I'm fueled by a strong passion for data analysis.
+    Presently, I'm immersed in the dynamic world of IT as an IT Support Technician at Kamoa Copper SA, part of the Ivanohemines Group. 
+    My role involves providing robust technical support and solutions to ensure smooth operations.
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me! 
+"""
+st.info(content2)
+
+
+
